471654856c9364717d36b8e166f6a22905b70850
Gbolahan Adekoye <Adekoyegbolahan001@gmail.com>
2023-01-16 13:37:20 +0100

    starter project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_flutter_apps/news_change_notifier.dart';
+import 'package:testing_flutter_apps/news_page.dart';
+import 'package:testing_flutter_apps/news_service.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      home: ChangeNotifierProvider(
+        create: (_) => NewsChangeNotifier(NewsService()),
+        child: const NewsPage(),
+      ),
+    );
+  }
+}
---
+++ lib/news_change_notifier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:testing_flutter_apps/article.dart';
+import 'package:testing_flutter_apps/news_service.dart';
+
+
+class NewsChangeNotifier extends ChangeNotifier {
+  final NewsService _newsService;
+
+  NewsChangeNotifier(this._newsService);
+
+  List<Article> _articles = [];
+
+  List<Article> get articles => _articles;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  Future<void> getArticles() async {
+    // TODO: Implement
+  }
+}
