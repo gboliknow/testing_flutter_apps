32fa293e9d2f761f77c7f2dbb13a13a82878d2c8
Gbolahan Adekoye <Adekoyegbolahan001@gmail.com>
2023-01-19 14:47:00 +0100

    first widget test

---
+++ test/news_page_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_flutter_apps/article.dart';
+import 'package:testing_flutter_apps/news_change_notifier.dart';
+import 'package:testing_flutter_apps/news_page.dart';
+import 'package:testing_flutter_apps/news_service.dart';
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late MockNewsService mockNewsService;
+
+  setUp(() {
+    mockNewsService = MockNewsService();
+  });
+
+  final articlesFromService = [
+    Article(title: 'Test 1', content: 'Test 1 content'),
+    Article(title: 'Test 2', content: 'Test 2 content'),
+    Article(title: 'Test 3', content: 'Test 3 content'),
+  ];
+
+  void arrangeNewsServiceReturns3Articles() {
+    when(() => mockNewsService.getArticles()).thenAnswer(
+      (_) async => articlesFromService,
+    );
+  }
+
+  Widget createWidgetUnderTest() {
+    return MaterialApp(
+      title: 'News App',
+      home: ChangeNotifierProvider(
+        create: (_) => NewsChangeNotifier(mockNewsService),
+        child: const NewsPage(),
+      ),
+    );
+  }
+
+  testWidgets(
+    'title is displayed',
+    (WidgetTester tester) async {
+      arrangeNewsServiceReturns3Articles();
+      await tester.pumpWidget(createWidgetUnderTest());
+      expect(find.text('News'), findsOneWidget);
+    },
+  );
+}
